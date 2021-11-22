af3fb252966d0f93f00ae6318900bab9240e6f9a
Dejan Vujacic <vujacicdejan98@gmial.com>
2021-11-22 15:41:48 +0100

    Kreirana navigacija na home.php strani, kao i CSS za tu stranu

---
+++ home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>TeamUp</title>
+</head>
+<body>
+    <header>
+            
+            <div class="navbar">
+            <section id="logo">
+                <h1><span class="logo">TeamUp</span></h1>
+            </section>
+            <div class="subnav">
+                <button class="subnavbtn">Profile <i class="fa fa-caret-down"></i></button>
+                 <div class="subnav-content">
+                    <a href="#">Log out</a>
+                    <a href="#MatchHistory">Match History</a>
+                    <a href="#Profile">Profile</a>
+                </div>
+            </div>
+            <div class="subnav">
+                <button class="subnavbtn">Games <i class="fa fa-caret-down"></i></button>
+                <div class="subnav-content">
+                    <a href="#Valorant">Valorant</a>
+                    <a href="#Dota2">Dota 2</a>
+                    <a href="#R6Siege">R6Siege</a>
+                    <a href="#LOL">LOL</a>
+                    <a href="#CS:GO">CS:GO</a>
+                </div>
+            </div>
+                <a href="#about">About</a>
+                <a href="#home">Home</a> 
+            </div>
+            
+        
+    </header>
+    
+</body>
+</html>
